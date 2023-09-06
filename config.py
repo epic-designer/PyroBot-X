9d697b14cdebf90afab0f6ab2760ae9be9c1fa9f
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-06 07:51:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-OWNER_ID = os.getenv("OWNER_ID", "1238234357")
+OWNER_ID = os.getenv("OWNER_ID")
 HANDLER = ["~", ".","!","?","@","$"]
 PyroX = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1001964645198")
-SOURCE = "https://t.me/Dark_nemesis0"
+SOURCE = "https://t.me/Sexy_Dark"
 LOG_CHAT = -1001964645198
